7dde302c1273433d0625b64f30f5dc30070a8974
hushirui <hushirui-hb@bestpay.com.cn>
2018-09-06 21:07:11 +0800

    .

--- itswr/src/main/java/com/mrathena/windfall/itswr/task/initialize/HttpInitTask.java
+++ itswr/src/main/java/com/mrathena/windfall/itswr/task/initialize/HttpInitTask.java
@@ -1,7 +1,9 @@
 package com.mrathena.windfall.itswr.task.initialize;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.cache.CacheManager;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,14 @@ import org.springframework.stereotype.Component;
 @Order(value = 1)
 public class HttpInitTask implements ApplicationRunner {
 
+	@Autowired
+	private CacheManager cache;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("初始化任务");
+		System.out.println(cache.getCache("https"));
+		System.out.println(cache.getCacheNames());
 	}
 
 }
